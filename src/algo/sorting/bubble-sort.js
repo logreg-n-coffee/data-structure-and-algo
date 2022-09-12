b3ad53e3627885d338f21988ae47f64b92609780
Rui Hu <hurui141@gmail.com>
2022-09-12 11:38:57 -0700

    Add two examples for time complexity calculation

--- src/algo/sorting/bubble-sort.js
+++ src/algo/sorting/bubble-sort.js
@@ -20,29 +20,43 @@ const bubbleSortAscending = input => {
     // create a switch variable
     let swapping = true;
 
+    // logging the swapping time(s)
+    let swappingTime = 0;
+
     while (swapping) {
         // turn off the switch first
         swapping = false;
-
+        
         // loop from the first element to the second-to-last element 
         for (let i = 0; i < input.length - 1; i++) {
             // compare the value 
             if (input[i] > input[i + 1]) {
+                // operate on the swapping
                 console.log(`swapping pair ${input[i]} and ${input[i + 1]} in array [${input}]`);
                 swap(input, i, i + 1);
+
+                // increment of the swapping time
+                swappingTime++;
+
                 // keep the while loop running to see if additional swaps might be needed
                 swapping = true;
             }
         }
     }
+
+    console.log(`Swapped ${swappingTime} times.`);
 };
 
 // the algo has a time complexity of O(n^2); the optimal case has an Omega(n)
 
 // testing
 
-let testArr = [5, 4, 3, 2, 1];
-console.log(testArr);
+const arr1 = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+console.log("before sorting", arr1);
+bubbleSortAscending(arr1);
+console.log("after sorting", arr1);
 
-bubbleSortAscending(testArr);
-console.log(testArr);
+const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("before sorting", arr2);
+bubbleSortAscending(arr2);
+console.log("after sorting", arr2);
