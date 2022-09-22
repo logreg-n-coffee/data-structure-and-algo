81abb21c487d2b5ac6fb54fab38cdb4117e22ec1
Rui Hu <hurui141@gmail.com>
2022-09-21 17:29:05 -0700

    Update binary-search for leetcode submission

--- src/algo/search/binary-search.js
+++ src/algo/search/binary-search.js
@@ -45,3 +45,31 @@ if (number === target) {
 }
 
 // Time complexity: O(log n)
+
+// The above algorithm will exceed the time limit in Leetcode - see the following improved code
+
+const search = (nums, target) => {
+  let l = 0;
+  let r = nums.length - 1;
+
+  while (l <= r) {
+    let i = Math.floor((l + r) / 2);
+
+    if (nums[i] === target) {
+      return i;
+    } else if (nums[i] < target) {
+      l = i + 1;
+    } else {
+      r = i - 1;
+    }
+  }
+  return -1; // if target doesn't exist return -1
+};
+
+console.log('Leetcode Accepted Answer');
+
+const nums = [-1, 0, 3, 5, 9, 12];
+const numsTarget = 9;
+
+const result = search(nums, numsTarget);
+console.log('result', result);
